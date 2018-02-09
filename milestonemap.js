35dea26f3baceb07c13a12288141ee641aaeb14a
paulmtalley <35548903+paulmtalley@users.noreply.github.com>
2018-02-08 19:44:17 -0700

    Update milestonemap.js

--- milestonemap.js
+++ milestonemap.js
@@ -1,1 +1,11 @@
-milestone3
+function initialize() {
+  var louisianaCoast = new google.maps.LatLng(29.774189,-92.060160)
+  var mapOptions = {
+    zoom: 8,
+    center: louisianaCoast,
+    mapTypeId: google.maps.MapTypeId.SATELLITE
+  };
+  var map = new google.maps.Map(
+      document.getElementById("map_canvas"),
+      mapOptions);
+}
